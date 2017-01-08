f439c209fda0e36a01ab5fc8fef137e624ec3e49
Anurag Nandan <anuragnan@gmail.com>
2017-01-08 18:19:27 -0500

    Making pattern argument mandatory and fixing PATH for youtube script

--- findmyphone.sh
+++ findmyphone.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
-#Run this process to find your phone. This process takes an argument to search for a pattern over all the devices connected devices to the wifi router.
-#Giving the mac address of the phone is suggested.
-
 
 SUBNET=`ip addr show | grep eth0 | grep inet | awk -F " " {'print $2'}`
-PATH=`pwd`
 PATTERN="$1"
-if [ $PATTERN -eq "" ]
+PATH=$(/usr/bin/dirname "$0")
+if [ -z "$PATTERN" ]
 then
-  echo "No pattern given to search. Requires an argument for searching"
-  exit 1
+	echo "Needs an argument (MAC Address of the device) of the device to detect if it is home or connected."
+	exit 1
 fi
-
 while [ 1 ]; do
-  OUT=`/usr/bin/sudo nmap -p 5353 -sV -O $SUBNET | /bin/grep $PATTERN`
-
-  if [ -z "$OUT" ]
-  then
-    echo "Not found.\n"
-		`/bin/sleep 5`
-  else
-    echo "Found.\n"
-    `/usr/bin/sudo php $PATH/youtube-castnow.php & > /dev/null`
-    exit 1
-fi
+	echo "Scanning for $PATTERN availability."
+        OUT=`/usr/bin/sudo nmap -p 5353 -sV -O $SUBNET | /bin/grep $PATTERN`
+        if [ -z "$OUT" ]
+                then
+			echo "Not found. Sleeping\n"
+			`/bin/sleep 5`
+       		else
+			echo "Found\n"
+			`/bin/sleep 3`
+			`/usr/bin/sudo php $PATH/youtube-castnow.php & > /dev/null &`
+		exit 1
+ fi
 done
